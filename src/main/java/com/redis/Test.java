1fa6c4ed35d74fba202186b35c59a4dbdda1d9de
zhongding <zhongding@cenboomh.com>
2019-07-21 13:05:39 +0800

    Revert "ide"
    This reverts commit 72de3a15

--- src/main/java/com/redis/Test.java
+++ src/main/java/com/redis/Test.java
@@ -7,6 +7,6 @@ package com.redis;
  */
 public class Test {
     public static void main(String[] args) {
-        System.out.println("hello test1 irde");
+        System.out.println("hello test1");
     }
 }
